971f64730ac90d2f7e98c3715e8de5e50dfd7cdf
Nithish Siliveru <nithishkumar.sileveri123@gmail.com>
2023-04-08 21:02:18 +0530

    l2

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import "./TaskCard.css";
 interface TaskCardInterface {
-      title: string,
-      completedAtDate?: string,
-      dueDate?: string,
-      assigneeName: string
-  }
-const TaskCard = (props:TaskCardInterface) => {
-  console.log(props)
-  return (
-    <div className='TaskItem border-black'>
-      <h3 className="text-xl font-semibold border-black">{props.title}</h3>
-     {props.dueDate &&<p> Due on:{props.dueDate}</p>}
-      {props.completedAtDate && <p> Completed On:{props.completedAtDate}</p>}
-
-      <p>Assignee: {props.assigneeName}</p>
-    </div>
-  )
+  title?: string;
+  completedAtDate?: string;
+  dueDate?: string;
+  assigneeName?: string;
 }
+const TaskCard = (props: TaskCardInterface) => {
+  console.log(props);
+  return (
+    <div className="TaskItem border-black">
+      <h3 className="text-xl font-semibold border-black">{props.title}</h3>
+      {props.dueDate && <p> Due on:{props.dueDate}</p>}
+      {props.completedAtDate && <p> Completed On:{props.completedAtDate}</p>}
+      {props.assigneeName && <p>Assignee: {props.assigneeName}</p>}
+    </div>
+  );
+};
 
-export default TaskCard
+export default TaskCard;
