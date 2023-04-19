e90caf5d0deacc04bc0e94ac5e23a043fabed03d
nithish1018 <nithishsiliveru18@gmail.com>
2023-04-19 15:59:06 +0530

    level 3 changes

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,20 +1,24 @@
 import "./TaskCard.css";
+import React from 'react';
 interface TaskCardInterface {
   title: string;
   completedAtDate?: string;
   dueDate?: string;
   assigneeName: string;
+  description?:string;
 }
-const TaskCard = (props: TaskCardInterface) => {
-  console.log(props);
-  return (
-    <div className="TaskItem border-black">
-      <p className="text-xl font-semibold border-black">{props.title}</p>
-      {props.dueDate && <p> Due on: {props.dueDate}</p>}
-      {props.completedAtDate && <p> Completed on: {props.completedAtDate}</p>}
-      <p>Assignee: {props.assigneeName}</p>
-    </div>
-  );
-};
+class TaskCard extends React.Component<TaskCardInterface> {
+  render() {
+    return (
+      <div className="TaskItem shadow-md border border-slate-100">
+        <h2 className="text-base font-bold my-1">{this.props.title}</h2>
+        <p className="text-sm text-slate-500">{this.props.dueDate}</p>
+        <p className="text-sm text-slate-500">
+          Description: {this.props.description}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default TaskCard;
---
+++ smarter-tasks/src/Task.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface TaskProp {
+  title: string;
+}
+class Task extends React.Component<TaskProp> {
+  render() {
+    return (
+      <div>{this.props.title}</div>
+    )
+  }
+}
+export default Task;
