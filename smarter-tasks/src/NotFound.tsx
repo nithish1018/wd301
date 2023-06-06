6e5df38f3c213a2bd7fab6b0f73742f6ade2df83
nithish1018 <nithishsiliveru18@gmail.com>
2023-06-06 11:15:49 +0530

    Added all level 5 requirements

--- smarter-tasks/src/NotFound.tsx
+++ smarter-tasks/src/NotFound.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const NotFound: React.FC = () => {
   return (
     <div>
-      <h1>Task Manager</h1>
-      <p>Welcome to the Task Manager application!</p>
+      <h1 className='px-4'>404
+      Page doesn't exists</h1>
+     <Link to='/' className="ml-6 text-gray-300 hover:text-white"> <button
+          
+          id="backToHomeButton"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Go Home
+        </button>
+        </Link>
     </div>
   );
 };
