a1731ee2fd4d3ec74289969aa8e75aa9c988be60
nithish1018 <nithishsiliveru18@gmail.com>
2023-06-09 18:46:45 +0530

    Added signout facility

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
+ const userData = JSON.parse(localStorage.getItem('userData')|| '{}');
+ console.log(userData.name);
+ console.log(userData.email);
+ const logout = async () => {
+  localStorage.removeItem('userData');
+
+ }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
+      <Link to='/' id='logout-link' className="ml-6 text-gray-300 hover:text-white"> <button
+          
+          id="backToHomeButton"
+          onClick={logout}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+         Log Out
+        </button>
+        </Link>
     </div>
   );
 }
--- smarter-tasks/src/pages/signup/SignupForm.tsx
+++ smarter-tasks/src/pages/signup/SignupForm.tsx
@@ -8,6 +8,8 @@ const SignupForm: React.FC = () => {
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
+
+
   const nav=useNavigate();
 //   const authToken = localStorage.getItem('authToken');
 
@@ -33,6 +35,7 @@ const SignupForm: React.FC = () => {
       localStorage.setItem('authToken', jsonData.token);
     //   localStorage.setItem('userData', jsonData.user);
     localStorage.setItem('userData', jsonData.data);
+
    
       nav("/dashboard");
     } catch (error) {
