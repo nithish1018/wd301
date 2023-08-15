727ac0130972aab1e817fffe3ad5a05b07a64270
nithish1018 <nithishsiliveru18@gmail.com>
2023-08-15 11:27:06 +0530

    Fixed 404 error

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -1,35 +1,34 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
-import AccountLayout from "../layouts/account"
-import NewTask from "../pages/tasks/NewTask";
-import ProtectedRoute from "../ProtectedRoute"
-import ProjectDetails from "../pages/project_details";
-import ProjectContainer from "../pages/projects/ProjectContainer";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import Signin from "../pages/signin"
-import Signup from "../pages/signup"
-import Projects from "../pages/projects"
-import Members from "../pages/members"
-import Notfound from "../pages/Notfound";
+import AccountLayout from "../layouts/account";
+import ProtectedRoute from "./ProtectedRoutes";
+import Signin from "../pages/signin";
+import Signup from "../pages/signup";
+import Projects from "../pages/projects";
+import Members from "../pages/members";
 import Logout from "../pages/logout";
+import ProjectContainer from "../pages/projects/ProjectContainer";
+import ProjectDetails from "../pages/project_details";
+import NewTask from "../pages/tasks/NewTask";
 import TaskDetailsContainer from "../pages/tasks/TaskDetailsContainer";
 
 const router = createBrowserRouter([
-    { path: "/", element: <Navigate to="/account/projects" replace /> },
-    {
-      path: "/", 
-      element: <Signin />
-    },
+  { path: "/", element: <Navigate to="/account/projects" replace /> },
   {
-    path: "/signin", 
-    element: <Signin />
+    path: "/",
+    element: <Signin />,
   },
   {
-    path: "/signup", 
-    element: <Signup />
+    path: "/signin",
+    element: <Signin />,
   },
-  { 
-    path: "/logout", 
-    element: <Logout /> 
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/logout",
+    element: <Logout />,
   },
   // Protected Routes
   {
@@ -40,52 +39,41 @@ const router = createBrowserRouter([
       </ProtectedRoute>
     ),
     children: [
-        { index: true, element: <Navigate to="/account/projects" replace /> },
-        {
-          path: "projects",
-          element: <ProjectContainer />,
-          children: [
-            { index: true, element: <Projects /> },
-            {
-              path: "projects",
-              element: <ProjectContainer />,
-              children: [
-                { index: true, element: <Projects /> },
-                {
-                  path: ":projectID",
-                  element: <ProjectDetails />,
-                  children: [
-                    { index: true, element: <></> },
-                    {
-                      path: "tasks",
-                      children: [
-                        { index: true, element: <Navigate to="../" /> },
-                        {
-                          path: "new",
-                          element: <NewTask />,
-                        },
-                        {
-                          path: ":taskID",
-                          children: [
-                            { index: true, element: <TaskDetailsContainer /> },
-                          ],
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
+      { index: true, element: <Navigate to="/account/projects" replace /> },
       {
-        path: "members",
-        element: (<Members />)
+        path: "projects",
+        element: <ProjectContainer />,
+        children: [
+          { index: true, element: <Projects /> },
+          {
+            path: ":projectID",
+            element: <ProjectDetails />,
+            children: [
+              { index: true, element: <></> },
+              {
+                path: "tasks",
+                children: [
+                  { index: true, element: <Navigate to="../" /> },
+                  {
+                    path: "new",
+                    element: <NewTask />,
+                  },
+                  {
+                    path: ":taskID",
+                    children: [
+                      { index: true, element: <TaskDetailsContainer /> },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       },
       {
-        path:"*",
-        element:<Notfound/>
-      }
+        path: "members",
+        element: <Members />,
+      },
     ],
   },
 ]);
